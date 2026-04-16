df80a698249e0554917db3321a8999505868b463
winsznx <winsznx@users.noreply.github.com>
2026-04-17 00:04:42 +0100

    feat(functional): add barrel export for the functional module

---
+++ apps/web/src/lib/functional/index.ts
@@ -0,0 +1,7 @@
+export { pipe } from "./pipe";
+export { compose } from "./compose";
+export { memoize } from "./memoize";
+export { assertNever } from "./assert-never";
+export { typedKeys, typedEntries } from "./typed-keys";
+export { match } from "./match";
+export { deepFreeze } from "./deep-freeze";
