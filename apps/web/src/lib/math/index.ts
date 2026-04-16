559f8e500e2cae52a9ad2e9312ab5fb359544030
winsznx <winsznx@users.noreply.github.com>
2026-04-16 23:51:25 +0100

    feat(math): add barrel export aggregating the math module helpers

---
+++ apps/web/src/lib/math/index.ts
@@ -0,0 +1,9 @@
+export { BPS_SCALE, bpsToFraction, fractionToBps, applyBps } from "./bps";
+export { apyFromDailyRate, apyFromPeriodRate, aprToApy } from "./apy";
+export { applySlippage, minAmountOut, maxAmountIn } from "./slippage";
+export { utilizationRatio, collateralRatio } from "./tvl-ratio";
+export { priceImpact, priceImpactBps } from "./price-impact";
+export { compoundInterest, continuousCompound } from "./compound";
+export { percentageChange, percentageChangeBps } from "./percentage-change";
+export { clamp, clamp01 } from "./clamp";
+export { lerp, inverseLerp } from "./lerp";
