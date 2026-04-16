a166fdb47ea60b4d9bfcc6bdd5e796667ec53db6
winsznx <winsznx@users.noreply.github.com>
2026-04-16 23:51:25 +0100

    feat(tx): add barrel export for the tx module

---
+++ apps/web/src/lib/tx/index.ts
@@ -0,0 +1,4 @@
+export { TX_STATUS, type TxStatus, isTerminalTxStatus } from "./status";
+export { parseTxError } from "./error-parse";
+export { trackConfirmations, type ConfirmationProgress } from "./confirmations";
+export { shortTxId } from "./short-id";
