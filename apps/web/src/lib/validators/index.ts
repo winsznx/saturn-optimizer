7b257e1e1d241e86826a21d0414f8c9282ad319f
winsznx <winsznx@users.noreply.github.com>
2026-04-16 23:42:28 +0100

    feat(validators): add barrel export aggregating validator guards

---
+++ apps/web/src/lib/validators/index.ts
@@ -0,0 +1,7 @@
+export { isStacksAddress } from "./stacks-address";
+export { isEvmAddress } from "./evm-address";
+export { isEvmTxHash, isStacksTxId } from "./tx-hash";
+export { isStacksPrincipal } from "./principal";
+export { isPositiveAmount, isNonNegativeAmount } from "./amount";
+export { isHttpUrl } from "./url";
+export { isPercentage, isUnitInterval } from "./percentage";
