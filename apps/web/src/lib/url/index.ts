5299f96afe37801b5f767bec48e30c9331b37608
winsznx <winsznx@users.noreply.github.com>
2026-04-16 23:42:28 +0100

    feat(url): add barrel export for the url module

---
+++ apps/web/src/lib/url/index.ts
@@ -0,0 +1,5 @@
+export { hiroExplorerUrl, type HiroResource } from "./hiro-explorer";
+export { stacksApiUrl, type StacksNetwork } from "./stacks-api";
+export { buildQueryString, parseQueryString } from "./query-params";
+export { stacksAddressLink, stacksTxLink } from "./address-link";
+export { joinUrl } from "./join";
