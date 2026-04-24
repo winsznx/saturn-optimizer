397b98a0d8d2795fc7642a80baa4530ccbfeb1e7
winsznx <winsznx@users.noreply.github.com>
2026-04-24 19:08:22 +0000

    refactor(web): optimize cn utility with spread operator

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -2,7 +2,7 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(...inputs));
 }
 
 export function truncateAddress(addr: string | undefined, start = 6, end = 4): string {
