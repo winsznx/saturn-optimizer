3a384af6f893f39e51cdd1a1842480c0c6e37b54
winsznx <winsznx@users.noreply.github.com>
2026-04-27 10:35:51 +0100

    chore(lib): remove unused lib/url module (no in-tree consumers; explorer.ts is canonical)

--- apps/web/src/lib/url/address-link.ts
+++
@@ -1,9 +0,0 @@
-import { hiroExplorerUrl } from "./hiro-explorer";
-
-export function stacksAddressLink(address: string, testnet = false): string {
-  return hiroExplorerUrl("address", address, testnet ? "testnet" : "mainnet");
-}
-
-export function stacksTxLink(txid: string, testnet = false): string {
-  return hiroExplorerUrl("txid", txid, testnet ? "testnet" : "mainnet");
-}
--- apps/web/src/lib/url/join.ts
+++
@@ -1,9 +0,0 @@
-export function joinUrl(...segments: string[]): string {
-  return segments
-    .map((segment, index) => {
-      if (index === 0) return segment.replace(/\/+$/, "");
-      return segment.replace(/^\/+|\/+$/g, "");
-    })
-    .filter((segment) => segment.length > 0)
-    .join("/");
-}
